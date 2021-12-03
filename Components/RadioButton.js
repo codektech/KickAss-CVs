2067fa9156a663f76b86ee5c4d5c0bdf95f6cbc1
IbtahajAli <88149767+IbtahajAli@users.noreply.github.com>
2021-12-03 02:16:52 -0800

    radio button dn

--- Components/RadioButton.js
+++ Components/RadioButton.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { View, TouchableOpacity } from 'react-native'
 
 const RadioButton = () => {
+    const [color, setcolor] = useState()
     return (
         <View style={{flexDirection:'row'}}>
             <TouchableOpacity
-                style={{width:15,height:15,borderRadius:40,borderColor:'#2EBCAE',borderWidth:1}}
+                onPress={()=>setcolor(!color)}
+                style={{width:15,height:15,borderRadius:40,borderColor:'#2EBCAE',borderWidth:1,backgroundColor:color?"#807F7F":null}}
             />
         </View>
     );
