3d68c3c22f2fbe3384a6d659d7adebcd9d53fe83
IbtahajAli <88149767+IbtahajAli@users.noreply.github.com>
2021-12-03 02:16:42 -0800

    Header dn

--- Components/Header.js
+++ Components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text} from 'react-native'
 
 const Header = () => {
     return (
         <View style={{backgroundColor:'#2EBCAE',borderTopRightRadius:20,padding:'3%'}}>
-                <Text style={{color:'#fff',fontSize:20,fontWeight:'bold'}}>Jane Roe</Text>
-                <Text style={{color:'#fff',fontSize:16,borderBottomWidth:1,borderColor:'#fff'}}>Buisness development Manager</Text>
-                <Text style={{color:'#fff',fontSize:13,top:'5%'}}>
+                <Text style={{color:'#fff',fontSize:16,fontWeight:'bold'}}>Jane Roe</Text>
+                <Text style={{color:'#fff',fontSize:12,}}>Buisness development Manager</Text>
+                <View style={{width:'72%',borderBottomWidth:1,borderColor:'#fff'}}/>
+                <Text style={{color:'#fff',fontSize:10,top:'5%'}}>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                     Ipsum has been the industry's standard dummy text ever since the 1500s, 
                 </Text>
